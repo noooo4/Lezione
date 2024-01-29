e7b2015efef806f44454c85f81bdc8604aa28071
noooo4 <126702983+noooo4@users.noreply.github.com>
2024-01-29 11:50:20 +0100

    Rinominato esercitazione
    Ho rinominato Esercitazione in Esercitazione Calcolatrice

---
+++ Esercitazione Calcolatrice/esercitazione2/js/script.js
@@ -0,0 +1,7 @@
+let nome = window.prompt ("inserisci il tuo nome") ;
+let eta= Number (window.prompt("anno di nascita?") );
+
+let diff=2023-eta;
+
+document.write("CIAO "+ nome + "<br>");
+document.write("differenza: "+ diff + "<br>");
